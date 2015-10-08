fc62c7d943833dfddaa265739d209cadfe1aea54
André Luiz Banki <banki@altoqi.com.br>
2015-10-07 21:50:26 -0300

    Verificação de corredor válido no login - TODO: está lendo as corridas duas vezes (na Principal e na Main)

--- app/src/main/java/br/g4/senairunner/senairunner/activity/Principal.java
+++ app/src/main/java/br/g4/senairunner/senairunner/activity/Principal.java
@@ -1,5 +1,6 @@
 package br.g4.senairunner.senairunner.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,10 +11,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import br.g4.senairunner.senairunner.MainActivity;
 import br.g4.senairunner.senairunner.R;
+import br.g4.senairunner.senairunner.dominio.Corrida;
+import br.g4.senairunner.senairunner.service.CorridaService;
 
 public class Principal extends AppCompatActivity {
+
+    List<Corrida> corridas;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +30,66 @@ public class Principal extends AppCompatActivity {
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        texto = (TextView) findViewById(R.id.edtinscricao);
     }
 
     public void onClickProcurar(View v){
-        TextView texto = (TextView) findViewById(R.id.edtinscricao);
         if (texto.length() > 0){
             String numero = texto.getText().toString();
-            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putString("numero", numero);
-            editor.commit();
+            obtemCorridas(numero);
+        } else
+            alertValidacao("Número da inscrição não preenchido");
+    }
 
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(Principal.this, "Número da inscrição não preenchido", Toast.LENGTH_LONG).show();
-            texto.requestFocus();
-        }
+    private void obtemCorridas(final String numero) {
+        final ProgressDialog progressDlg = ProgressDialog.show(
+                Principal.this,
+                getString(R.string.app_name),
+                "Obtendo registros...");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    corridas = CorridaService.getCorridas(numero);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressDlg.dismiss();
+                            if (corridas.size() == 0)
+                                alertValidacao("Esse corredor não está cadastrado em nenhuma corrida");
+                            else
+                                redirecionaParaListaCorridas(numero);
+                        }
+                    });
+                } catch (Exception e){
+                    corridas = null;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressDlg.dismiss();
+                            alertValidacao("Não existe nenhum corredor com esse número");
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void redirecionaParaListaCorridas(String numero) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("numero", numero);
+        editor.commit();
+
+        Intent intent = new Intent(Principal.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void alertValidacao(String mensagem) {
+        Toast.makeText(Principal.this, mensagem, Toast.LENGTH_LONG).show();
+        texto.requestFocus();
     }
 
 }
