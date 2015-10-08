8498c446379e5377cc6f137520f692ad3aa65d81
André Luiz Banki <banki@altoqi.com.br>
2015-10-07 22:19:11 -0300

    Refatoração

--- app/src/main/java/br/g4/senairunner/senairunner/activity/Principal.java
+++ app/src/main/java/br/g4/senairunner/senairunner/activity/Principal.java
@@ -18,10 +18,12 @@ import br.g4.senairunner.senairunner.R;
 import br.g4.senairunner.senairunner.dominio.Corrida;
 import br.g4.senairunner.senairunner.service.CorridaService;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements Runnable {
 
     List<Corrida> corridas;
+    String numero;
     TextView texto;
+    ProgressDialog progressDlg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,45 +38,39 @@ public class Principal extends AppCompatActivity {
 
     public void onClickProcurar(View v){
         if (texto.length() > 0){
-            String numero = texto.getText().toString();
-            obtemCorridas(numero);
+            numero = texto.getText().toString();
+            progressDlg = ProgressDialog.show(Principal.this, getString(R.string.app_name),
+                    "Obtendo registros...");
+            Thread t = new Thread(this, "getCorridas");
+            t.start();
         } else
             alertValidacao("Número da inscrição não preenchido");
     }
 
-    private void obtemCorridas(final String numero) {
-        final ProgressDialog progressDlg = ProgressDialog.show(
-                Principal.this,
-                getString(R.string.app_name),
-                "Obtendo registros...");
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    corridas = CorridaService.getCorridas(numero);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            progressDlg.dismiss();
-                            if (corridas.size() == 0)
-                                alertValidacao("Esse corredor não está cadastrado em nenhuma corrida");
-                            else
-                                redirecionaParaListaCorridas(numero);
-                        }
-                    });
-                } catch (Exception e){
-                    corridas = null;
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            progressDlg.dismiss();
-                            alertValidacao("Não existe nenhum corredor com esse número");
-                        }
-                    });
+    @Override
+    public void run() {
+        try {
+            corridas = CorridaService.getCorridas(numero);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (corridas.size() == 0)
+                        alertValidacao("Esse corredor não está cadastrado em nenhuma corrida");
+                    else
+                        redirecionaParaListaCorridas(numero);
                 }
-            }
-        }).start();
+            });
+        } catch (Exception e){
+            corridas = null;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    alertValidacao("Não existe nenhum corredor com esse número");
+                }
+            });
+        } finally {
+            progressDlg.dismiss();
+        }
     }
 
     private void redirecionaParaListaCorridas(String numero) {
